c5a9d409f16efab61b2c5916d2cf2d6ed7e36de7
anicazulch <31353283+anicazulch@users.noreply.github.com>
2017-09-22 09:05:04 -0700

    Lab 5 Day 2

--- DemocracyBot.java
+++ DemocracyBot.java
@@ -3,7 +3,7 @@ import kareltherobot.*;
 
 
 /**
- * @author :  D. Appel
+ * @author :  A. Zulch
  */
 public class DemocracyBot extends Robot
 {
@@ -12,8 +12,112 @@ public class DemocracyBot extends Robot
     }
     
     public void fixBallots() {
-        
+        checkBallot();
+        returnToPosition();
+        checkBallot();
+        returnToPosition();
+        checkBallot();
+        returnToPosition();
+        checkBallot();
+        returnToPosition();
+        checkBallot();
     }
-   
+    public void checkBallot() {
+        move();
+        if (nextToABeeper()){
+            placeBallot();
+        }
+        else {
+            pickUpBallot();
+        }
+    }
+    public void placeBallot() {
+        faceNorth();
+        while (frontIsClear()){
+            move();
+        }
+        if (nextToABeeper()){
+            faceSouth();
+            while (frontIsClear()){
+                move();
+            }
+        }
+        else {
+            faceSouth();
+            while (!nextToABeeper()){
+                putBeeper();
+            }
+            while (frontIsClear()){
+                move();
+            }
+        }
+        if (nextToABeeper()){
+            faceNorth();
+            move();
+        }
+        else {
+            while (!nextToABeeper()){
+                putBeeper();
+            }
+            faceNorth();
+            move();
+        }
+    }
+    public void pickUpBallot(){
+        faceNorth();
+        while (frontIsClear()){
+            move();
+        }
+        if (!nextToABeeper()){
+            faceSouth();
+            while (frontIsClear()){
+                move();
+            }
+        }
+        else {
+            faceSouth();
+            while (nextToABeeper()){
+                pickBeeper();
+            }
+            while (frontIsClear()){
+                move();
+            }
+        }
+        if (!nextToABeeper()){
+            faceNorth();
+            move();
+        }
+        else {
+            while (nextToABeeper()){
+                pickBeeper();
+            }
+            faceNorth();
+            move();
+        }
+    }
+    public void returnToPosition() {
+        faceEast();
+        move();
+    }
+    public void faceNorth(){
+        while (!facingNorth()){
+            turnLeft();
+        }
+    }
+     public void faceEast(){
+        while (!facingEast()){
+            turnLeft();
+        }
+    }
+      public void faceSouth(){
+        while (!facingSouth()){
+            turnLeft();
+        }
+    }
+      public void faceWest(){
+        while (!facingWest()){
+            turnLeft();
+        }
+    }
 }
 
